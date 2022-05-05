fc8d514f749c3d0ffe110ebea3f7d609b9101a5f
1238497 <lleonard.avalos@est.fib.upc.edu>
2022-05-05 09:54:58 +0200

    muslito goes brrrrrrrr

--- Assets/MuslitoScript.cs
+++ Assets/MuslitoScript.cs
@@ -1,30 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class MuslitoScript : MonoBehaviour
 {
-    public Vector3 posInicial, posFinal;
+    public GameObject gameObjInicio, gameObjFinal;
     public float velocidad;
     private Vector3 DirectionNormalized;
-    private Vector3 RotationAngle;
+    public enum MuslitoType { Tp_al_inicio, boomerang };
+    public MuslitoType mus;
+    private Vector3 posIni, posFin;
+    private float distance, distance_traveled;
 
+    private bool direction;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.transform.position = posInicial;
-        DirectionNormalized = Vector3.Normalize(posFinal - posInicial);
 
-        RotationAngle = new Vector3(0, Vector3.Angle(DirectionNormalized, new Vector3(1, 0, 0)), 0);
-        gameObject.transform.eulerAngles = RotationAngle;
+        posIni = gameObjInicio.GetComponent<Transform>().position;
+        posFin = gameObjFinal.GetComponent<Transform>().position;
+        gameObject.transform.position = posIni;
+        DirectionNormalized = Vector3.Normalize(posFin - posIni);
+
+
+        distance = Vector3.Distance(posFin,posIni);
+        distance_traveled = 0;
+        // Convert to -180 to +180 degrees
+        transform.LookAt(gameObjFinal.GetComponent<Transform>());
+        transform.Rotate(new Vector3(0, 90,0));
+
+        gameObjInicio.GetComponent<MeshRenderer>().enabled = false;
+        gameObjFinal.GetComponent<MeshRenderer>().enabled = false;
+
+        direction = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position += DirectionNormalized * velocidad * Time.deltaTime;
-        RotationAngle.z += Time.deltaTime * velocidad * -360;
-        gameObject.transform.eulerAngles = RotationAngle;
+
+
+
+        if (mus == MuslitoType.boomerang) {
+            if (direction)
+            {
+                transform.Rotate(new Vector3(0, 0, Time.deltaTime * velocidad * 360));
+                gameObject.transform.position += DirectionNormalized * velocidad * Time.deltaTime;
+                distance_traveled += velocidad * Time.deltaTime;
+                if (distance_traveled > distance)
+                {
+                    direction = !direction;
+                }
+            }
+            else {
+
+
+                transform.Rotate(new Vector3(0, 0, Time.deltaTime * velocidad * -360));
+                gameObject.transform.position -= DirectionNormalized * velocidad * Time.deltaTime;
+                distance_traveled -= velocidad * Time.deltaTime;
+
+                if (distance_traveled < 0)
+                {
+                    direction = !direction;
+                }
+            }
+        }
+        else {
+
+            transform.Rotate(new Vector3(0, 0, Time.deltaTime * velocidad * 360));
+            gameObject.transform.position += DirectionNormalized * velocidad * Time.deltaTime;
+            distance_traveled += velocidad * Time.deltaTime;
+            if (distance_traveled > distance) {
+                distance_traveled = 0;
+                transform.position = posIni;
+            }
+            
+        }
+
+
+
     }
 }
