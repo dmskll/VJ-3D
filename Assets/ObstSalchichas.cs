08927a77aeb5221325ddbf7f933546070e8de05b
dmskll <danimolleja@protonmail.com>
2022-05-06 20:16:29 +0200

    add molino vertical

---
+++ Assets/ObstSalchichas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstSalchichas : MonoBehaviour
+{
+    public float speed;
+    public GameObject inicio, final;
+    Transform t_ini, t_final;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t_ini = inicio.GetComponent<Transform>();
+        t_final = inicio.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
