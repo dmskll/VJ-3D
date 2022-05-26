c9fa5413c1c6cc5a7170ebd62600a656d47811bb
dmskll <danimolleja@protonmail.com>
2022-05-26 21:24:31 +0200

    tocar cola

---
+++ Assets/Models/player/fixtail1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class fixtail1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Mesh mesh = GetComponent<SkinnedMeshRenderer>().sharedMesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
