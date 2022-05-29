f6dd30741c9c97f4f8f9017e64341c1fe8bea617
dmskll <danimolleja@protonmail.com>
2022-05-29 10:45:43 +0200

    remove unused models

---
+++ Assets/Scripts/EndScreenScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScreenScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //check who won and enable texts etc etc
+    }
+
+
+    public void clickMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void clickNextLevel()
+    {
+        int num = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(num + 1);
+    }
+}
---
+++ Assets/Scripts/circleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class circleController : MonoBehaviour
+{
+    public Image circulo, centro;
+    public Text text;
+
+    bool ready;
+    bool start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        centro.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setReady(bool b)
+    {
+        if (b)
+        {
+            centro.enabled = true;
+            text.enabled = false;
+
+        }
+        else
+        {
+            centro.enabled = false;
+            text.enabled = true;
+        }
+    }
+
+    public void setStart()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
